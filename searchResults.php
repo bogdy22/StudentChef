fe5124f10bc79ebdf5abb9624f3f83adc9463423
Sam Hirst <SamuelJHirst@outlook.com>
2021-03-18 20:15:45 +0000

    removed unused files, broken javascript and duplicate html code

--- searchResults.php
+++ searchResults.php
@@ -10,14 +10,7 @@
       	
       	
       ?>
-    <title>StudentChef</title>
-    <meta charset="utf-8">
-   
-    <link href="searchStyles.css" rel="stylesheet" >
-    <link rel="stylesheet" href="style.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
-   
+    <link href="searchStyles.css" rel="stylesheet" >   
   </head>
 <body>
    	<?php include("nav.php") ?>
