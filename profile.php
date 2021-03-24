05f250a2089ed4d81df20c93b6d08956a9c7df60
Sam Hirst <SamuelJHirst@outlook.com>
2021-03-24 21:53:18 +0000

    created profile view - including way to change name and postcode

---
+++ profile.php
@@ -0,0 +1,117 @@
+<?php
+	require("api/users.php");
+	require("api/recipes.php");
+?>
+
+<?php 
+	session_start();
+	$_SESSION["returnPath"] = "../profile.php?id=$_GET[id]";
+?>
+
+<?php
+	if (!empty($_GET["id"])) {
+		$user = getUserByID($_GET["id"])[1];
+	}
+
+	if (empty($user)) {
+		echo("Uh oh!");
+		die();
+	}
+
+	$recipes = getRecipeByUser($_GET["id"])[1];
+?>
+
+<?php
+	if (!empty($_POST["detailsName"]) && !empty($_POST["detailsPostcode"])) {
+		if (isset($_SESSION["userID"]) && $_SESSION["userID"] == $_GET["id"]) {
+			updateUser($_GET["id"], $_POST["detailsName"], $user["CASName"], $_POST["detailsPostcode"]);
+			$user["PreferredName"] = $_POST["detailsName"];
+			$user["Postcode"] = $_POST["detailsPostcode"];
+		} else {
+			echo("Uh oh!");
+			die();
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php include("head.html") ?>
+		<link rel="stylesheet" href="recipeStyles.css">
+	</head>
+	<body>
+	<?php include("nav.php") ?>
+	<div class="container">
+		<div class="row" id="title">
+			<div class="col p-3">
+				<header>
+					<h1><?php echo($user["PreferredName"]); ?></h1>
+					<?php 
+						if (isset($_SESSION["userID"]) && $_SESSION["userID"] == $_GET["id"]) {
+							echo("<a href='#!' data-toggle='modal' data-target='#modal-change-details'>Change Details?</a>");
+						}
+					?>
+				</header>
+			</div>
+		</div>
+		<div class="row" id="main">
+			<div class="col-12">
+				<main>
+					<section>
+						<h2>User's Recipes</h2>
+						<div id="recipes">
+							<?php
+								foreach ($recipes as $recipe) {
+									echo("
+										<div class='row'>
+											<div class='col-10'>
+												<h4>$recipe[Name]</h4>
+												<p>$recipe[Description]</p>
+											</div>
+											<div class='col-2'>
+												<a class='btn btn-primary right' href='recipe.php?id=$recipe[ID]'>View</a>
+											</div>
+										</div>
+									");
+								}
+							?>
+						</div>
+					</section>
+				</main>
+			</div>	
+		</div>
+		<div id="modal-change-details" class="modal fade" tabindex="-1" role="dialog">
+			<div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title">Change Details</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<div id="alert" class="alert alert-danger" role="alert" style="display: none;">Please provide a preferred name and postcode.</div>
+						<?php echo("<form id='change-details-form' method='POST' action='profile.php?id=$_GET[id]'>"); ?>
+						<div class="form-group">
+							<label for="detailsName">Preferred Name</label>
+							<?php echo("<input type='text' class='form-control' id='detailsName' name='detailsName' value='$user[PreferredName]'>"); ?>
+						</div>
+						<div class="form-group">
+							<label for="detailsPostcode">Postcode</label>
+							<?php echo("<input type='text' class='form-control' id='detailsPostcode' name='detailsPostcode' value='$user[Postcode]'>"); ?>
+							<small id="postcodeSubtext" class="form-text text-muted">This is used for the ingredient matching service.</small>
+						</div>
+						</form>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-primary" id="profile-submit">Submit</button>
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<script src="scripts.js"></script>
+		<script src="profileScripts.js"></script>
+	</body>
+</html>
